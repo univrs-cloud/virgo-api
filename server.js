881402e9453d6fd621e881b08bbc5bae2dfcd1c2
Robert <robert@robert.univrs>
2024-03-26 23:31:25 +0200

    Disable API route, not used for now

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '..', '..', 'virgo-ui/dist/index.html'));
 });
 
-app.use('/api/', routes);
+// app.use('/api/', routes);
 
 app.use((req, res, next) => {
 	res.status(404).send({ error: 'Not found.' });
