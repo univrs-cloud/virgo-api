634e14152bcc4d7820553f575638db7c17a30c79
Chindris Adrian <chindrisady1994@gmail.com>
2024-07-30 14:23:23 +0300

    Increse polling time (#2)
    * Add drive temperature endpoint
    
    * Increse polling time
    
    ---------
    
    Co-authored-by: Adrian Chindris <adrian.chindris@syneto.eu>

--- emitters/host.js
+++ emitters/host.js
@@ -224,6 +224,31 @@ const pollStorage = () => {
 		});
 };
 
+const pollDrives = () => {
+	if (nsp.server.engine.clientsCount === 0) {
+		delete state.drives;
+		return;
+	}
+
+	exec("smartctl --scan | awk '{print $1}' | xargs -I {} smartctl -a -j {} | jq -s .")
+		.then((response) => {
+			let stdout = JSON.parse(response.stdout);
+			state.drives = stdout.map((drive) => {
+				return {
+					name: drive.device.name,
+					temperature: drive.temperature.current
+				};
+			});
+		})
+		.catch((error) => {
+			state.drives = false;
+		})
+		.then(() => {
+			nsp.emit('drives', state.drives);
+			setTimeout(pollDrives, 60000);
+		});
+};
+
 const pollNetwork = () => {
 	if (nsp.server.engine.clientsCount === 0) {
 		delete state.network;
@@ -334,6 +359,11 @@ module.exports = (io) => {
 			nsp.emit('storage', state.storage);
 		} else {
 			pollStorage();
+		}
+		if (state.drives) {
+			nsp.emit('drives', state.drives);
+		} else {
+			pollDrives();
 		}
 		if (state.network) {
 			nsp.emit('network', state.network);
