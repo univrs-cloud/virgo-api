6d78a5d8ce0caca17c1bb526338cacff85c853ca
Adrian Chindris <chindrisady1994@gmail.com>
2024-07-31 13:48:19 +0300

    Get shares

--- emitters/share.js
+++ emitters/share.js
@@ -1,12 +1,46 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const ini =  require('ini');
+
 let nsp;
 let state = {};
 
-module.exports = (io) => {
-	state.shares = [];
+const pollShares = () => {
+	if (nsp.server.engine.clientsCount === 0) {
+		delete state.shares;
+		return;
+	}
 
+	exec('testparm -s -l')
+		.then((response) => {
+			let shares = ini.parse(response.stdout);
+			delete shares.global;
+			state.shares = [];
+			for (let [name, value] of Object.entries(shares)) {
+				let share = {
+					name: name,
+					isPrivate: value['guest ok']?.toLowerCase() !== 'yes',
+					cap: 0
+				};
+				state.shares.push(share);
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			state.shares = false;
+		})
+		.then(() => {
+			nsp.emit('shares', state.shares);
+			setTimeout(pollShares, 2000);
+		});
+};
+
+module.exports = (io) => {
 	nsp = io.of('/share').on('connection', (socket) => {
 		if (state.shares) {
 			nsp.emit('shares', state.shares);
+		} else {
+			pollShares();
 		}
 
 		socket.on('disconnect', () => {
