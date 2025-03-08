8540a38aeef9371536863220a91d891d0a9864e6
Robert <robert.gurde@gmail.com>
2025-03-08 15:40:59 +0200

    Clear the account cookie value if session is terminated

--- server.js
+++ server.js
@@ -18,8 +18,16 @@ app.set('trust proxy', true);
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
+	const SIX_MONTHS_MS = 1000 * 60 * 60 * 24 * 184;
+	const cookieOptions = {
+		domain: req.hostname,
+		encode: String,
+		httpOnly: false,
+		secure: true,
+		sameSite: 'lax',
+		maxAge: SIX_MONTHS_MS
+	};
 	if (req.headers['remote-user']) {
-		const SIX_MONTHS_MS = 1000 * 60 * 60 * 24 * 184;
 		let account = {
 			name: req.headers['remote-name'],
 			user: req.headers['remote-user'],
@@ -27,16 +35,9 @@ app.use((req, res, next) => {
 			groups: req.headers['remote-groups']?.split(',')
 		};
 		const serializedAccount = Buffer.from(JSON.stringify(account)).toString('base64');
-		res.cookie('account', serializedAccount, {
-			domain: req.hostname,
-			encode: String,
-			httpOnly: false,
-			secure: true,
-			sameSite: 'lax',
-			maxAge: SIX_MONTHS_MS
-		});
+		res.cookie('account', serializedAccount, cookieOptions);
 	} else {
-		res.clearCookie('account');
+		res.cookie('account', '', cookieOptions);
 	}
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
