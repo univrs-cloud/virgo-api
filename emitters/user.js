7f2a1b6caba54e398b9cdbb7efea24863e093fa3
Robert <robert.gurde@gmail.com>
2025-03-29 15:22:51 +0200

    Change user password

--- emitters/user.js
+++ emitters/user.js
@@ -1,7 +1,15 @@
+const fs = require('fs');
+const util = require('util');
+const childProcess = require('child_process');
+const exec = util.promisify(childProcess.exec);
+const yaml = require('js-yaml');
+const bcrypt = require('bcryptjs');
 const linuxUser = require('linux-sys-user').promise();
+const cost = 12;
 
 let nsp;
 let state = {};
+const autheliaUsersFile = '/messier/apps/authelia/config/users.yml';
 
 Promise.all(
 	[
@@ -22,6 +30,43 @@ Promise.all(
 		state.users = false;
 	});
 
+const updateProfile = (socket, config) => {
+	if (!socket.isAuthenticated) {
+		return;
+	}
+
+	//
+};
+
+const changePassword = (socket, config) => {
+	if (!socket.isAuthenticated) {
+		return;
+	}
+
+	linuxUser.setPassword(socket.user, config.password)
+		.then(() => {
+			setSambaUserPasswrd(socket.user, config.password);
+			setAutheliaUserPassword(socket.user, config.password);
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+	
+	function setSambaUserPasswrd(username, password) {
+		// `smbpasswd -a "${username}" < <(printf "%s\n%s\n" "${password}" "${password}")`
+	}
+
+	function setAutheliaUserPassword(username, password) {
+		const fileContents = fs.readFileSync(autheliaUsersFile, { encoding: 'utf8', flag: 'r' });
+		let usersConfig = yaml.load(fileContents);
+		if (usersConfig.users && usersConfig.users[username]) {
+			usersConfig.users[username].password = bcrypt.hashSync(password, cost);
+			const updatedYaml = yaml.dump(usersConfig, { indent: 2 });
+			fs.writeFileSync(autheliaUsersFile, updatedYaml, 'utf8', { flag: 'w' });
+		}
+	}
+};
+
 module.exports = (io) => {
 	nsp = io.of('/user');
 	nsp.use((socket, next) => {
@@ -35,6 +80,9 @@ module.exports = (io) => {
 		if (socket.isAuthenticated) {
 			nsp.to(`user:${socket.user}`).emit('users', state.users);
 		}
+
+		socket.on('profile', (config) => { updateProfile(socket, config); });
+		socket.on('password', (config) => { changePassword(socket, config); });
 
 		socket.on('disconnect', () => {
 			//
