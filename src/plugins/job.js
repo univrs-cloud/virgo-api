c9899c6e30816a80067aed636f4954a2a793fe9d
Robert <robert.gurde@gmail.com>
2025-09-16 19:47:40 +0300

    Job plugin

--- src/plugins/job.js
+++ src/plugins/job.js
@@ -1,36 +1,12 @@
-const { Queue, QueueEvents } = require('bullmq');
 const BasePlugin = require('./base');
 
 class JobPlugin extends BasePlugin {
-	#queues = ['configuration-jobs', 'host-jobs', 'docker-jobs', 'user-jobs', 'share-jobs'];
-
 	constructor(io) {
 		super(io, 'job');
-		this.#watchQueues();
 	}
 
-	#watchQueues() {
-		const eventsToListen = ['waiting', 'progress'];
-		this.#queues.forEach((queueName) => {
-			const queue = new Queue(queueName);
-			const queueEvents = new QueueEvents(queueName);
-			eventsToListen.forEach((event) => {
-				queueEvents.on(event, async (response) => {
-					try {
-						let job = await queue.getJob(response.jobId);
-						if (job) {
-							for (const socket of this.getNsp().sockets.values()) {
-								if (socket.isAuthenticated && socket.isAdmin) {
-									this.getNsp().to(`user:${socket.username}`).emit('job', job);
-								}
-							}
-						}
-					} catch (error) {
-						console.error(`Error processing job ${response.jobId}:`, error);
-					}
-				});
-			});
-		});	
+	init() {
+		this.queues = ['configuration-jobs', 'host-jobs', 'docker-jobs', 'user-jobs', 'share-jobs'];
 	}
 }
 
---
+++ src/plugins/job/watcher.js
@@ -0,0 +1,28 @@
+
+const { Queue, QueueEvents } = require('bullmq');
+
+module.exports = {
+	register(plugin) {
+        const eventsToListen = ['waiting', 'progress'];
+		plugin.queues.forEach((queueName) => {
+			const queue = new Queue(queueName);
+			const queueEvents = new QueueEvents(queueName);
+			eventsToListen.forEach((event) => {
+				queueEvents.on(event, async (response) => {
+					try {
+						let job = await queue.getJob(response.jobId);
+						if (job) {
+							for (const socket of plugin.getNsp().sockets.values()) {
+								if (socket.isAuthenticated && socket.isAdmin) {
+									plugin.getNsp().to(`user:${socket.username}`).emit('job', job);
+								}
+							}
+						}
+					} catch (error) {
+						console.error(`Error processing job ${response.jobId}:`, error);
+					}
+				});
+			});
+		});
+	}
+};
