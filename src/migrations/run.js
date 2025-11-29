d5ff5fbdaaea7ac1827ff74306138f0aafa1f8d2
Robert <robert.gurde@gmail.com>
2025-11-29 03:19:39 +0200

    Fix missing async in migrations

--- src/migrations/run.js
+++ src/migrations/run.js
@@ -1,13 +1,15 @@
-console.log(`Running migrations...`);
-try {
-	await require('./000_setup')();
-	await require('./001_move_database_location')();
-	await require('./002_migrate_configuration')();
-	await require('./003_migrate_data')();
-	await require('./004_update_notification_configuration_files')();
-	
-	console.log(`All migrations completed successfully!`);
-} catch (error) {
-	console.error(`Migration failed:`, error);
-	throw error;
-}
+(async () => {
+	console.log(`Running migrations...`);
+	try {
+		await require('./000_setup')();
+		await require('./001_move_database_location')();
+		await require('./002_migrate_configuration')();
+		await require('./003_migrate_data')();
+		await require('./004_update_notification_configuration_files')();
+		
+		console.log(`All migrations completed successfully!`);
+	} catch (error) {
+		console.error(`Migration failed:`, error);
+		throw error;
+	}
+})();
