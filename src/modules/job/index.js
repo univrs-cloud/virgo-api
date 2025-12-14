e0870385e27a41c05d3ba6c746575840602153a9
Robert <robert.gurde@gmail.com>
2025-12-14 20:35:40 +0200

    Add missing key for jobs

--- src/modules/job/index.js
+++ src/modules/job/index.js
@@ -1,7 +1,7 @@
 const BaseModule = require('../base');
 
 class JobModule extends BaseModule {
-	#queues = ['configuration-jobs', 'host-jobs', 'docker-jobs', 'bookmark-jobs', 'user-jobs', 'share-jobs'];
+	#queues = ['configuration-jobs', 'host-jobs', 'docker-jobs', 'bookmark-jobs', 'user-jobs', 'share-jobs', 'metrics-jobs'];
 
 	constructor() {
 		super('job');
