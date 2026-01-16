21c570de74227e84cadaa8e5ab143d45324605dc
Robert <robert.gurde@gmail.com>
2026-01-16 17:08:04 +0200

    Remove unneded migrations

--- src/migrations/run.js
+++ src/migrations/run.js
@@ -1,11 +1,11 @@
 (async () => {
 	console.log(`Post install running...`);
 	try {
-		await require('./000_configure_zram')();
-		await require('./001_move_database_location')();
-		await require('./005_rename_configuration_order_table')(); // must run before migrate configuration and data
-		await require('./002_migrate_configuration')();
-		await require('./003_migrate_data')();
+		// await require('./000_configure_zram')();
+		// await require('./001_move_database_location')();
+		// await require('./005_rename_configuration_order_table')(); // must run before migrate configuration and data
+		// await require('./002_migrate_configuration')();
+		// await require('./003_migrate_data')();
 		await require('./004_update_notification_configuration_files')();
 		
 		console.log(`Post install completed successfully!`);
