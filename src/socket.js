c28d79e1caeec8fe84736e86bb1c42fe82f6816c
Robert <robert.gurde@gmail.com>
2026-01-27 20:09:59 +0200

    Websocket allow cookies

--- src/socket.js
+++ src/socket.js
@@ -8,7 +8,11 @@ const initializeSocket = (server) => {
 	}
 	
 	io = new Server(server, {
-		path: '/api'
+		path: '/api',
+		cors: {
+			origin: true,
+			credentials: true
+		}
 	});
 
 	return io;
