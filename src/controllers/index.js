fb07a66b3201f40d324023ba3551b045aeda1fc4
Robert <robert.gurde@gmail.com>
2026-01-27 21:30:30 +0200

    Add rest api for auth

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,7 +2,10 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 const staticController = require('./static');
+const authController = require('./auth');
 
+// API routes (before static middleware)
+router.use('/api/auth', authController);
 router.use('/', staticController.staticMiddleware);
 router.get(/.*/, (req, res) => {
 	res.sendFile(path.join(staticController.folderPath, 'index.html'));
