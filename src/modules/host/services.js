3a837e74ba38289ac4957617b7fdf58f40757895
Robert <robert.gurde@gmail.com>
2026-03-22 12:37:16 +0200

    Add system service enable,disable,start,stop functionality

--- src/modules/host/services.js
+++ src/modules/host/services.js
@@ -61,18 +61,74 @@ const loadServices = async (module) => {
 	}
 };
 
+const broadcastServices = async (module) => {
+	await loadServices(module);
+	for (const socket of module.nsp.sockets.values()) {
+		if (socket.isAuthenticated && socket.isAdmin) {
+			socket.emit('host:system:services', module.getState('services'));
+		}
+	}
+};
+
+const enableService = async (job, module) => {
+	const { config } = job.data;
+	const actionVerbs = module.nlp.conjugate('enable');
+	await module.updateJobProgress(job, `${config.serviceName} is ${actionVerbs.gerund}...`);
+	await execa('systemctl', ['enable', config.serviceName]);
+	module.eventEmitter.emit('host:system:services:updated');
+	return `${config.serviceName} ${actionVerbs.pastTense}.`;
+};
+
+const enableStartService = async (job, module) => {
+	const { config } = job.data;
+	await module.updateJobProgress(job, `${config.serviceName} is enabling and starting...`);
+	await execa('systemctl', ['enable', '--now', config.serviceName]);
+	module.eventEmitter.emit('host:system:services:updated');
+	return `${config.serviceName} enabled and started.`;
+};
+
+const disableService = async (job, module) => {
+	const { config } = job.data;
+	const actionVerbs = module.nlp.conjugate('disable');
+	await module.updateJobProgress(job, `${config.serviceName} is ${actionVerbs.gerund}...`);
+	await execa('systemctl', ['disable', config.serviceName]);
+	module.eventEmitter.emit('host:system:services:updated');
+	return `${config.serviceName} ${actionVerbs.pastTense}.`;
+};
+
+const disableStopService = async (job, module) => {
+	const { config } = job.data;
+	await module.updateJobProgress(job, `${config.serviceName} is disabling and stopping...`);
+	await execa('systemctl', ['disable', '--now', config.serviceName]);
+	module.eventEmitter.emit('host:system:services:updated');
+	return `${config.serviceName} disabled and stopped.`;
+};
+
+const startService = async (job, module) => {
+	const { config } = job.data;
+	const actionVerbs = module.nlp.conjugate('start');
+	await module.updateJobProgress(job, `${config.serviceName} is ${actionVerbs.gerund}...`);
+	await execa('systemctl', ['start', config.serviceName]);
+	module.eventEmitter.emit('host:system:services:updated');
+	return `${config.serviceName} ${actionVerbs.pastTense}.`;
+};
+
+const stopService = async (job, module) => {
+	const { config } = job.data;
+	const actionVerbs = module.nlp.conjugate('stop');
+	await module.updateJobProgress(job, `${config.serviceName} is ${actionVerbs.gerund}...`);
+	await execa('systemctl', ['stop', config.serviceName]);
+	module.eventEmitter.emit('host:system:services:updated');
+	return `${config.serviceName} ${actionVerbs.pastTense}.`;
+};
+
 const register = (module) => {
 	loadServices(module);
 
 	module.eventEmitter
-			.on('host:system:services:updated', async () => {
-				await loadServices(module);
-				for (const socket of module.nsp.sockets.values()) {
-					if (socket.isAuthenticated && socket.isAdmin) {
-						socket.emit('host:system:services', module.getState('services'));
-					}
-				}
-			});
+		.on('host:system:services:updated', async () => {
+			await broadcastServices(module);
+		});
 };
 
 const onConnection = (socket, module) => {
@@ -82,22 +138,73 @@ const onConnection = (socket, module) => {
 		}
 	}
 
-	socket.on('host:system:services:fetch', async () => { 
+	socket.on('host:system:services:fetch', async () => {
 		if (!socket.isAuthenticated || !socket.isAdmin) {
 			return;
 		}
 
-		await loadServices(module);
-		for (const socket of module.nsp.sockets.values()) {
-			if (socket.isAuthenticated && socket.isAdmin) {
-				socket.emit('host:system:services', module.getState('services'));
-			}
+		await broadcastServices(module);
+	});
+
+	socket.on('host:system:service:enable', async (config) => {
+		if (!socket.isAuthenticated || !socket.isAdmin) {
+			return;
 		}
+
+		await module.addJob('host:system:service:enable', { config, username: socket.username });
+	});
+
+	socket.on('host:system:service:enable-start', async (config) => {
+		if (!socket.isAuthenticated || !socket.isAdmin) {
+			return;
+		}
+
+		await module.addJob('host:system:service:enable-start', { config, username: socket.username });
+	});
+
+	socket.on('host:system:service:disable', async (config) => {
+		if (!socket.isAuthenticated || !socket.isAdmin) {
+			return;
+		}
+
+		await module.addJob('host:system:service:disable', { config, username: socket.username });
+	});
+
+	socket.on('host:system:service:disable-stop', async (config) => {
+		if (!socket.isAuthenticated || !socket.isAdmin) {
+			return;
+		}
+
+		await module.addJob('host:system:service:disable-stop', { config, username: socket.username });
+	});
+
+	socket.on('host:system:service:start', async (config) => {
+		if (!socket.isAuthenticated || !socket.isAdmin) {
+			return;
+		}
+
+		await module.addJob('host:system:service:start', { config, username: socket.username });
+	});
+
+	socket.on('host:system:service:stop', async (config) => {
+		if (!socket.isAuthenticated || !socket.isAdmin) {
+			return;
+		}
+
+		await module.addJob('host:system:service:stop', { config, username: socket.username });
 	});
 };
 
 module.exports = {
 	name: 'services',
 	register,
-	onConnection
+	onConnection,
+	jobs: {
+		'host:system:service:enable': enableService,
+		'host:system:service:enable-start': enableStartService,
+		'host:system:service:disable': disableService,
+		'host:system:service:disable-stop': disableStopService,
+		'host:system:service:start': startService,
+		'host:system:service:stop': stopService
+	}
 };
